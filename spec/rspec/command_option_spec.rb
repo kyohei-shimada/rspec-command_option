d5af19d4afa2c485f4f1fc7d1a93b2cf59615093
Kyohei Shimada <fl95to03pj30sq12@gmail.com>
2015-01-15 21:31:57 +0900

    implements some examples of rspec

--- spec/rspec/command_option_spec.rb
+++ spec/rspec/command_option_spec.rb
@@ -1,7 +1,61 @@
 require 'spec_helper'
 
-describe Rspec::CommandOption do
+describe RSpec::CommandOption do
   it 'has a version number' do
-    expect(Rspec::CommandOption::VERSION).not_to be nil
+    expect(RSpec::CommandOption::VERSION).not_to be nil
+  end
+
+  describe '#build' do
+    context 'without all configuration' do
+      subject(:command) { RSpec::CommandOption.new.build }
+
+      it { is_expected.to eq '' }
+    end
+
+    context 'with no arguments option' do
+      subject(:command) {
+        RSpec::CommandOption.new {|opt|
+          opt.color = true
+        }.build
+      }
+
+      it { is_expected.to eq '--color' }
+    end
+
+    context 'with argument option' do
+      subject(:command) {
+        RSpec::CommandOption.new {|opt|
+          opt.format = 'documentation'
+        }.build
+      }
+
+      it { is_expected.to eq '--format=documentation' }
+    end
+
+    context 'with multiple valid options' do
+      subject(:command) {
+        RSpec::CommandOption.new {|opt|
+          opt.color = true
+          opt.dry_run = true
+          opt.tag = "skip"
+        }.build
+      }
+
+      it {
+        is_expected.to include '--color'
+        is_expected.to include '--dry-run'
+        is_expected.to include 'tag=skip'
+      }
+    end
+
+    context 'with invalid options' do
+      subject(:command) {
+        RSpec::CommandOption.new {|opt|
+          opt.invalid_option = 'invalid_argument'
+        }.build
+      }
+
+      it { expect{command}.to raise_error NoMethodError }
+    end
   end
 end
