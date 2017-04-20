4c77a68874c0a488dcd59f2d17fa48b731b56c36
Kyohei Shimada <kyohei.shimada@bm-sms.jp>
2017-04-20 17:40:21 +0900

    integrated CircleCI and codecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rspec/command_option'
+
+require 'simplecov'
+SimpleCov.start
+
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
